46d27c450a3ec6372ffc763d03ee35e8635fbdc9
dianlujitao <dianlujitao@gmail.com>
2014-09-24 19:33:51 +0800

    mkvendor.sh: Initial commit of c8816

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := c8816
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/c8816/device_c8816.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := c8816
+PRODUCT_NAME := cm_c8816
+PRODUCT_BRAND := huawei
+PRODUCT_MODEL := c8816
+PRODUCT_MANUFACTURER := huawei
